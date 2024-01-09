25ef5532362f71d37b7fce35e767c94f1235aa3e
paladinomaker <brunopaladinomaker@gmail.com>
2024-01-08 22:24:58 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// Configurando o Git digitar no terminal
+//git config --global user.name "paladinomaker"
+//git config --global user.email "brunopaladinomaker@gmail.com"
+//git config --global credential.helper Store
+
+console.log("ola mundão")
